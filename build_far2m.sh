114fe1123cf06de29ca44ba2e3acd3b18bb7cab2
spvkgn <spvkgn@users.noreply.github.com>
2025-01-30 10:11:13 +0500

    update workflow

--- build_far2m.sh
+++ build_far2m.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eo pipefail
 
 REPO_DIR=$GITHUB_WORKSPACE
 export CCACHE_DIR=$REPO_DIR/.ccache
@@ -9,45 +10,46 @@ if [[ "$WXGUI" == "false" ]]; then
   CMAKE_OPTS+=( "-DUSEWX=no" )
 fi
 
-( [ -d $REPO_DIR/patches ] && cd $REPO_DIR/far2m && QUILT_PATCHES=$REPO_DIR/patches quilt push -a )
+[[ -d $REPO_DIR/patches ]] && ( cd $REPO_DIR/far2m && QUILT_PATCHES=$REPO_DIR/patches quilt push -a )
 
-mkdir -p $REPO_DIR/far2m/$BUILD_DIR && \
+mkdir -p $REPO_DIR/far2m/$BUILD_DIR
 cmake -S $REPO_DIR/far2m -B$REPO_DIR/far2m/$BUILD_DIR \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/usr \
   -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache \
   -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache \
-  ${CMAKE_OPTS[@]} && \
-  cmake --build $REPO_DIR/far2m/$BUILD_DIR --target install -- -j$(nproc) && \
+  ${CMAKE_OPTS[@]}
+  cmake --build $REPO_DIR/far2m/$BUILD_DIR --target install -- -j$(nproc)
 
 # build LuaFar
-mkdir -p $REPO_DIR/luafar2m/$BUILD_DIR && \
+mkdir -p $REPO_DIR/luafar2m/$BUILD_DIR
 cmake -S $REPO_DIR/luafar2m -B$REPO_DIR/luafar2m/$BUILD_DIR \
   -DHIGHLIGHT=yes \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=/usr && \
-cmake --build $REPO_DIR/luafar2m/$BUILD_DIR --target install -- -j$(nproc) && \
+  -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build $REPO_DIR/luafar2m/$BUILD_DIR --target install -- -j$(nproc)
 
-mkdir -p $REPO_DIR/standalone/lib && cp -a $REPO_DIR/far2m/$BUILD_DIR/install/* $REPO_DIR/standalone && \
-install -vm755 $REPO_DIR/AppRun $REPO_DIR/standalone && \
+mkdir -p $REPO_DIR/standalone/lib
+cp -a $REPO_DIR/far2m/$BUILD_DIR/install/* $REPO_DIR/standalone
+install -vm755 $REPO_DIR/AppRun $REPO_DIR/standalone
 # copy libs
 dpkg-query -L liblua5.1-0-dev libluajit-5.1-dev libonig-dev | grep -e 'liblua5.1.so' -e 'libluajit-5.1.so' -e 'libonig.so' |\
-  xargs -I{} cp -vL {} $REPO_DIR/standalone/lib && \
+  xargs -I{} cp -vL {} $REPO_DIR/standalone/lib
 # Lua Modules
-luarocks install moonscript --lua-version=5.1 && \
-luarocks install lrexlib-oniguruma --lua-version=5.1 && \
-install -vm644 /usr/local/lib/lua/5.1/*.so $REPO_DIR/standalone/lib && \
-cp -a /usr/local/share/lua $REPO_DIR/standalone && \
+luarocks install moonscript --lua-version=5.1
+luarocks install lrexlib-oniguruma --lua-version=5.1
+install -vm644 /usr/local/lib/lua/5.1/*.so $REPO_DIR/standalone/lib
+cp -a /usr/local/share/lua $REPO_DIR/standalone
 # LuaFar
 cp -na -t $REPO_DIR/standalone/Plugins/luafar \
   $REPO_DIR/AppDir/usr/lib/far2m/Plugins/luafar/* \
-  $REPO_DIR/AppDir/usr/share/far2m/Plugins/luafar/* && \
-cp -a $REPO_DIR/luafar2m/Macros $REPO_DIR/standalone && \
+  $REPO_DIR/AppDir/usr/share/far2m/Plugins/luafar/*
+cp -a $REPO_DIR/luafar2m/Macros $REPO_DIR/standalone
 # standalone
-( cd $REPO_DIR/standalone && ./far2m --help >/dev/null && bash -x $REPO_DIR/make_standalone.sh ) && \
+( cd $REPO_DIR/standalone && ./far2m --help >/dev/null && bash -x $REPO_DIR/make_standalone.sh )
 # bundle
-makeself --keep-umask --nomd5 --nocrc $REPO_DIR/standalone $PKG_NAME.run "FAR2M File Manager" ./AppRun && \
-tar cvf ${PKG_NAME/_${VERSION}}.run.tar $PKG_NAME.run && \
+makeself --keep-umask --nomd5 --nocrc $REPO_DIR/standalone $PKG_NAME.run "FAR2M File Manager" ./AppRun
+tar cvf ${PKG_NAME/_${VERSION}}.run.tar $PKG_NAME.run
 
 tar cJvf $REPO_DIR/far2m.tar.xz -C $REPO_DIR/AppDir .
 
